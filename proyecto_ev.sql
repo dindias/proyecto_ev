d9fba1f5339545e16c7442d178b315f27fa954e3
dindias <dani.indias-corral@epitech.eu>
2023-11-14 19:07:10 +0100

    BD

--- proyecto_ev.sql
+++ proyecto_ev.sql
@@ -3,12 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-11-2023 a las 13:03:58
+-- Tiempo de generación: 14-11-2023 a las 19:06:36
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
-DROP DATABASE proyecto_ev;
-CREATE DATABASE proyecto_ev;
-USE proyecto_ev;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -39,41 +37,57 @@ CREATE TABLE `coches` (
   `Potencia` int(11) NOT NULL,
   `Autonomia` int(11) NOT NULL,
   `Kilometraje` int(11) NOT NULL,
-  `Motorizacion` varchar(255) DEFAULT NULL,
-  `Contaminacion` varchar(255) DEFAULT NULL,
+  `Motorizacion` varchar(20) NOT NULL,
+  `Contaminacion` int(11) NOT NULL,
   `Precio` int(11) NOT NULL,
   `Tipo` varchar(50) DEFAULT NULL,
+  `ubicacion` varchar(30) NOT NULL,
   `Descripcion` varchar(255) DEFAULT NULL,
-  `Exterior` varchar(255) DEFAULT NULL,
-  `Interior` varchar(255) DEFAULT NULL,
-  `Seguridad` varchar(255) DEFAULT NULL,
-  `Tecnologia` varchar(255) DEFAULT NULL
+  `Exterior` text NOT NULL,
+  `Interior` text NOT NULL,
+  `Seguridad` text NOT NULL,
+  `Tecnologia` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `coches`
 --
 
-INSERT INTO `coches` (`CarID`, `UserID`, `Marca`, `Modelo`, `Ano`, `Matricula`, `Potencia`, `Autonomia`, `Descripcion`, `Precio`, `Tipo`) VALUES
-(47, 1, 'Ford', 'Focus', '2011', 'BCD2345', 200000, 'Necesita reparaciones menores', 4000, 'img/generic.jpg', ''),
-(49, 1, 'Honda', 'Accord', '2011', 'DEF4567', 120000, 'Pocos detalles de uso', 10000, 'img/generic.jpg', ''),
-(50, 1, 'Toyota', 'Corolla', '2011', 'EFG5678', 80000, 'Como nuevo, poco kilometraje', 15000, 'img/generic.jpg', ''),
-(51, 1, 'Toyota', 'Camry', '2011', 'FGH6789', 110000, 'Perfectas condiciones', 13000, 'img/generic.jpg', ''),
-(52, 1, 'Nissan', 'Sentra', '2011', 'GHI7890', 100000, 'Pequeños rasguños en pintura', 7500, 'img/generic.jpg', ''),
-(53, 1, 'Hyundai', 'Elantra', '2011', 'HIJ8901', 50000, NULL, 16000, 'img/generic.jpg', ''),
-(54, 1, 'Volkswagen', 'Golf', '2011', 'IJK9012', 140000, NULL, 7000, 'img/generic.jpg', ''),
-(55, 1, 'BMW', 'Series 3', '2011', 'JKL0123', 30000, 'Excelente estado, un solo dueño', 22000, 'img/generic.jpg', ''),
-(56, 1, 'Audi', 'A4', '2011', 'KLM1234', 90000, 'Pocos detalles de uso', 18000, 'img/generic.jpg', ''),
-(57, 1, 'Mercedes-Benz', 'C-Class', '2011', 'LMN2345', 10000, 'Como nuevo', 25000, 'img/generic.jpg', ''),
-(58, 1, 'Ford', 'Mustang', '2011', 'MNO3456', 40000, 'Perfectas condiciones, poco kilometraje', 26000, 'img/generic.jpg', ''),
-(59, 1, 'Chevrolet', 'Spark', '2011', 'NOP4567', 95000, NULL, 8000, 'img/generic.jpg', ''),
-(60, 1, 'Renault', 'Duster', '2011', 'OPQ5678', 65000, 'Buen estado, bien cuidado', 9000, 'img/generic.jpg', ''),
-(61, 1, 'Toyota', 'Yaris', '2011', 'JDH7777', 1, 'Nuevo', 30000, 'img/generic.jpg', ''),
-(62, 1, 'Ford', 'Focus', '2011', 'KKK7777', 2, '2', 25000, 'img/ford-focus-es-FORD_EXPLORER_3-21x9-2160x925-bb-blue-focus-parked-on-the-street.jpg.renditions.original.jpeg', ''),
-(68, 1, 'Lamborghini', 'Diablo', '2011', 'KYS6969', 2, 'Precioso', 150000, 'img/lamborghini_diablo_gt_1-1-1.jpeg', ''),
-(70, 1, 'BMW', 'M3', '2011', 'LLL9999', 2, 'Nuevo', 70000, 'img/1366_2000.jpeg', ''),
-(71, 1, 'Volkswagen', 'Polo', '2011', 'MMM2222', 2, 'Viejo', 5000, 'img/generic.jpg', ''),
-(75, 2, 'Citroen', 'Sara', '2000', 'XXX2222', 200000, 'Muy viejo', 2000, 'img/Citroën_Xsara_(Facelift)_–_Frontansicht,_17._März_2011,_Wülfrath.jpg', 'Compacto');
+INSERT INTO `coches` (`CarID`, `UserID`, `Marca`, `Modelo`, `Ano`, `Matricula`, `Potencia`, `Autonomia`, `Kilometraje`, `Motorizacion`, `Contaminacion`, `Precio`, `Tipo`, `ubicacion`, `Descripcion`, `Exterior`, `Interior`, `Seguridad`, `Tecnologia`) VALUES
+(82, 1, 'BMW', 'X3', '2023', 'AAA1111', 500, 800, 25000, 'Hibrida', 100, 600, 'Suv', 'Mérida', 'Nuevo', 'Negro', 'Beige', 'Es de hierro coño', 'ABS');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favoritos`
+--
+
+CREATE TABLE `favoritos` (
+  `FavoritoID` bigint(20) UNSIGNED NOT NULL,
+  `CarID` bigint(20) UNSIGNED NOT NULL,
+  `UserID` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagenes`
+--
+
+CREATE TABLE `imagenes` (
+  `ImagenID` bigint(20) UNSIGNED NOT NULL,
+  `CarID` bigint(20) UNSIGNED NOT NULL,
+  `UserID` bigint(20) UNSIGNED NOT NULL,
+  `Imagen` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `imagenes`
+--
+
+INSERT INTO `imagenes` (`ImagenID`, `CarID`, `UserID`, `Imagen`) VALUES
+(10, 82, 1, 'img/bmw-x3.jpg'),
+(11, 82, 1, 'img/bmw-x32.jpg');
 
 -- --------------------------------------------------------
 
@@ -89,15 +103,6 @@ CREATE TABLE `reservas` (
   `FechaFin` date NOT NULL,
   `Observaciones` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `reservas`
---
-
-INSERT INTO `reservas` (`ReservationID`, `UserID`, `CarID`, `FechaInicio`, `FechaFin`, `Observaciones`) VALUES
-(5, 2, 68, '2023-11-16', '2023-11-23', ''),
-(6, 2, 68, '2023-11-16', '2023-11-23', ''),
-(7, 2, 68, '2023-11-16', '2023-11-23', '');
 
 -- --------------------------------------------------------
 
@@ -139,6 +144,22 @@ ALTER TABLE `coches`
   ADD KEY `fk_usuarios` (`UserID`);
 
 --
+-- Indices de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD PRIMARY KEY (`FavoritoID`),
+  ADD KEY `CarID` (`CarID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indices de la tabla `imagenes`
+--
+ALTER TABLE `imagenes`
+  ADD PRIMARY KEY (`ImagenID`),
+  ADD KEY `CarID` (`CarID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
 -- Indices de la tabla `reservas`
 --
 ALTER TABLE `reservas`
@@ -162,7 +183,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `coches`
 --
 ALTER TABLE `coches`
-  MODIFY `CarID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+  MODIFY `CarID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
+
+--
+-- AUTO_INCREMENT de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  MODIFY `FavoritoID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `imagenes`
+--
+ALTER TABLE `imagenes`
+  MODIFY `ImagenID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `reservas`
@@ -187,33 +220,26 @@ ALTER TABLE `coches`
   ADD CONSTRAINT `fk_usuarios` FOREIGN KEY (`UserID`) REFERENCES `usuarios` (`UserID`);
 
 --
+-- Filtros para la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`CarID`) REFERENCES `coches` (`CarID`),
+  ADD CONSTRAINT `favoritos_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `usuarios` (`UserID`);
+
+--
+-- Filtros para la tabla `imagenes`
+--
+ALTER TABLE `imagenes`
+  ADD CONSTRAINT `imagenes_ibfk_1` FOREIGN KEY (`CarID`) REFERENCES `coches` (`CarID`),
+  ADD CONSTRAINT `imagenes_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `usuarios` (`UserID`);
+
+--
 -- Filtros para la tabla `reservas`
 --
 ALTER TABLE `reservas`
   ADD CONSTRAINT `fk_coches_reservas` FOREIGN KEY (`CarID`) REFERENCES `coches` (`CarID`),
   ADD CONSTRAINT `fk_usuarios_reservas` FOREIGN KEY (`UserID`) REFERENCES `usuarios` (`UserID`);
 COMMIT;
-
--- Crear la tabla "imagenes"
-CREATE TABLE `imagenes` (
-                            `ImagenID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-                            `CarID` bigint(20) UNSIGNED NOT NULL,
-                            `UserID` bigint(20) UNSIGNED NOT NULL,
-                            `Imagen` varchar(255) NOT NULL,
-                            PRIMARY KEY (`ImagenID`),
-                            FOREIGN KEY (`CarID`) REFERENCES `coches` (`CarID`),
-                            FOREIGN KEY (`UserID`) REFERENCES `usuarios` (`UserID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Crear la tabla "favoritos"
-CREATE TABLE `favoritos` (
-                             `FavoritoID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-                             `CarID` bigint(20) UNSIGNED NOT NULL,
-                             `UserID` bigint(20) UNSIGNED NOT NULL,
-                             PRIMARY KEY (`FavoritoID`),
-                             FOREIGN KEY (`CarID`) REFERENCES `coches` (`CarID`),
-                             FOREIGN KEY (`UserID`) REFERENCES `usuarios` (`UserID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
