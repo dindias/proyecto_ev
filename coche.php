c356630e8fa3cddf22eb9992959579150666fe25
dindias <dani.indias-corral@epitech.eu>
2023-11-22 12:22:19 +0100

    Las notificaciones se muestran en todas las páginas

--- coche.php
+++ coche.php
@@ -365,11 +365,15 @@ $car = getCar($carID);
     </div>
 </div>
 
+<div class="toast-container position-fixed bottom-0 end-0 p-3" data-user-id="<?php echo $_SESSION['user_id']?>">
+</div>
+
 <footer class="container-fluid text-center">
     <?php include("footer.php"); ?>
 </footer>
 
 <script src="coche.js"></script>
+<script src="notification.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
--- info.php
+++ info.php
@@ -173,7 +173,8 @@ include ("register.php");
     </div>
 </div>
 
-
+<div class="toast-container position-fixed bottom-0 end-0 p-3" data-user-id="<?php echo $_SESSION['user_id']?>">
+</div>
 
 <!--Section: FAQ-->
 
@@ -182,6 +183,7 @@ include ("footer.php");
 ?>
 
 <script src="info.js"></script>
+<script src="notification.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
